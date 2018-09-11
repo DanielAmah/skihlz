f975938f49ea3346651bb4cf81c4e6fb62a96b5a
DanielAmah <amahdanieljack@gmail.com>
2018-09-12 00:44:29 +0100

    setup app with rails 5.2 and ruby 2.5

--- Gemfile
+++ Gemfile
@@ -60,3 +60,15 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# jQuery
+gem 'jquery-rails'
+gem 'jquery-ui-rails'
+
+# Amazon Flexible Payments gem
+gem 'amazon_flex_pay'
+
+# Configuration File
+gem 'config'
+
+gem 'paypal-sdk-buttonmanager'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'preorder#index'
+  match '/preorder'               => 'preorder#index', :via => [:get,:post]
+  get 'preorder/checkout'
+  match '/preorder/share/:uuid'   => 'preorder#share', :via => :get
+  match '/preorder/ipn'           => 'preorder#ipn', :via => :post
+  match '/preorder/prefill'       => 'preorder#prefill', :via => [:get,:post]
+  match '/preorder/postfill'      => 'preorder#postfill', :via => [:get,:post]
 end
