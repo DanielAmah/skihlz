a80a52004659d30b4d569a7e522c1586d345583f
DanielAmah <amahdanieljack@gmail.com>
2018-09-13 03:03:54 +0100

    add amount to payment notifications

--- app/controllers/payment_notification_controller.rb
+++ app/controllers/payment_notification_controller.rb
@@ -2,8 +2,8 @@ class PaymentNotificationController < ApplicationController
   protect_from_forgery except: [:create]
 
   def create
-    if PaymentNotification.create!(params: params, order_id: params[:invoice], status: params[:payment_status], transaction_id: params[:txn_id])
-      redirect_to root_path, notice: 'Notification created'
+    if PaymentNotification.create!(params: params, order_id: params[:invoice], status: params[:payment_status], transaction_id: params[:txn_id], amount: params[:payment_gross])
+      redirect_to preorder_path, notice: 'Notification created'
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_07_010735) do
+ActiveRecord::Schema.define(version: 2018_10_07_010736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2018_10_07_010735) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.string "amount"
   end
 
   create_table "payment_options", force: :cascade do |t|
